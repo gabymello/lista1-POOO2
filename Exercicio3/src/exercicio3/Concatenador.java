95b6f6a9c262ff7fd12e85c0e4c69b49dbe5e338
gabymello <mellogaby689@gmail.com>
2025-09-01 17:33:06 -0300

    Add files via upload

---
+++ Exercicio3/src/exercicio3/Concatenador.java
@@ -0,0 +1,12 @@
+
+package exercicio3;
+
+
+public class Concatenador {
+  private String parte1 = "Ola,";
+  private String parte2 = "mundo!";
+
+   public String concatenar(){
+       return parte1 + parte2;
+   }
+}
---
+++ Exercicio3/src/exercicio3/Exercicio3.java
@@ -0,0 +1,14 @@
+
+package exercicio3;
+
+
+public class Exercicio3 {
+
+   
+    public static void main(String[] args) {
+      Concatenador c = new Concatenador();
+      String mensagem = c.concatenar();
+        System.out.println(mensagem);
+    }
+    
+}
